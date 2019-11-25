c036d12a0e54d050bd97906a24eee5b5a6ea7d01
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2019-11-25 16:01:33 +0100

    Update 404 error

--- data-portal-web/src/components/notfound.js
+++ data-portal-web/src/components/notfound.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Notfound = () => 
+const Notfound = () =>
     <div>
-        <h1>Not found</h1>
-        <p>The requested URL was not found on this server.</p>
+        <h1>404 - Page not found</h1>
+        <div className={'row'}>
+            <div className={'col-sm-10 offset-sm-1'}>
+                <p>Sorry, but the page you were trying to view does not exist.</p>
+                <p>It looks like this was the result of either:
+                    <ul>
+                        <li>a mistyped address</li>
+                        <li>an out-of-date link</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
     </div>;
 
 export default Notfound
