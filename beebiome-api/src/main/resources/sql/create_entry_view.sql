79dffa5e69d798f6bd3a30640935960872b0c5c0
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-09-14 18:10:07 +0200

    Add sql query to create entity view

---
+++ beebiome-api/src/main/resources/sql/create_entry_view.sql
@@ -0,0 +1,33 @@
+CREATE VIEW entity AS 
+(
+SELECT s.biosampleAcc AS biosampleAcc,
+       p.bioprojectAcc AS bioprojectAcc,
+       bp.id AS biosamplePackageId,
+       bp.name AS biosamplePackageName,
+       string_agg(DISTINCT e.libraryStrategy, ',') AS libraryStrategies,
+       p.submittingOrganizationName AS submittingOrganizationName,
+       string_agg(DISTINCT e.libraryLayout, ',') AS libraryLayouts,
+       string_agg(DISTINCT e.librarySource, ',') AS librarySources,
+       sp.id AS speciesId,
+       sp.scientificName AS speciesScientificName,
+       h.id AS hostId,
+       h.scientificName AS hostScientificName,
+       string_agg(DISTINCT e.platform, ',') AS platforms,
+       l.name AS geoLocName,
+       s.collectionDate AS collectionDate
+--        ,
+--        string_agg(DISTINCT stn.nucleotideLink, ',') AS nucleotideLinks
+
+FROM sample s
+         INNER JOIN projectToSample ps ON (ps.biosampleAcc = s.biosampleAcc)
+         INNER JOIN project p ON (p.bioprojectAcc = ps.bioprojectAcc)
+         INNER JOIN biosamplePackage bp ON (bp.id = s.biosamplePackageId)
+         INNER JOIN sampleToExperiment se ON (se.biosampleAcc = s.biosampleAcc)
+         INNER JOIN experiment e ON (e.sraAcc = se.sraAcc)
+         INNER JOIN species sp ON (sp.id = s.speciesId)
+         INNER JOIN species h ON (h.id = s.hostSpeciesId)
+         INNER JOIN location l ON (l.id = s.locationId)
+--          INNER JOIN sampleToNucleotide stn ON (stn.biosampleAcc = s.biosampleAcc)
+
+GROUP BY s.biosampleAcc, p.bioprojectAcc, bp.id, sp.id, h.id, l.name, s.collectionDate
+);
