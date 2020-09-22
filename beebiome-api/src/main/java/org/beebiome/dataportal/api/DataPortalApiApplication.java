44b5976588df834eb527794fdb07bb60cc0bb0e1
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-09-22 10:54:37 +0200

    Modify to get a war

--- beebiome-api/src/main/java/org/beebiome/dataportal/api/DataPortalApiApplication.java
+++ beebiome-api/src/main/java/org/beebiome/dataportal/api/DataPortalApiApplication.java
@@ -3,13 +3,21 @@ package org.beebiome.dataportal.api;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataPortalApiApplication {
+public class DataPortalApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DataPortalApiApplication.class);
 		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DataPortalApiApplication.class);
+	}
+
 }
