be490f8867cef688b462e3dc0086a0ff6fed9c00
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-10-13 17:28:58 +0200

    Add obfuscated e-mail adress to 'Contact us' link

--- beebiome-web/src/components/navs/footer.js
+++ beebiome-web/src/components/navs/footer.js
@@ -19,7 +19,7 @@ function Footer() {
                     </ul>
                     <ul className="nav navbar-nav navbar-right">
                         <li className="nav-item">
-                            <a href="mailto:null@beebiome.org">Contact us</a>
+                            <a href="javascript:location='mailto:\u0063\u006f\u006e\u0074\u0061\u0063\u0074\u0040\u0062\u0065\u0065\u0062\u0069\u006f\u006d\u0065\u002e\u006f\u0072\u0067';void 0">Contact us</a>
                         </li>
                     </ul>
                 </div>
