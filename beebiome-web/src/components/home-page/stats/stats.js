3e6a61fa4897d552fdaacd9729a6296fdf613073
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-10-20 10:48:30 +0200

    Update stats panel in home page

--- beebiome-web/src/components/home-page/stats/stats.js
+++ beebiome-web/src/components/home-page/stats/stats.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import ReactGA from "react-ga";
+import Loading from "../../result/loading";
 
-export default function Stats() {
-    return (
-        <section className="beebiome-stats">
-            <h2>Statistics</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
-                commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
-                porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-        </section>
-    );
+export default class Stats extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: null,
+            isLoaded: false,
+            errorMessage: null
+        };
+    }
+
+    componentDidMount() {
+        let self = this;
+        self.setState({data: null, isLoaded: false, errorMessage: null});
+
+        console.log("Fetching data...");
+        fetch(process.env.REACT_APP_API_URL + "/statistics/all")
+            .then(results => { return results.json()} )
+            .then(data => {
+                self.setState({data: data, isLoaded: true, errorMessage: null});
+            })
+            .catch(function(error) {
+                self.setState({errorMessage: "Failed to get data from API."});
+            });
+    }
+
+    render() {
+        let text = "";
+        if (this.state.isLoaded) {
+            text = "BeeBiome contains " + this.state.data.sampleCount + " BioSamples (entries), " +
+                "encompassing " + this.state.data.projectCount + " Bioprojects " +
+                "and " + this.state.data.experimentCount + " SRA experiments. " +
+                "This represents data for " + this.state.data.hostCount + " Apoidea hosts. " +
+                "If you use our data, please note the date of access, as data can change."
+        } else if (this.state.errorMessage !== null) {
+            text = <p>{this.state.errorMessage}</p>
+        } else {
+            text = <Loading/>
+        }
+
+        return (
+            <section className="beebiome-stats">
+                <h2>Statistics</h2>
+                <p>{text}</p>
+            </section>
+        );
+    }
 }
 
  
