f3d4a47561a11978daa6bfa4b880eb48a56eda2b
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-10-20 15:11:54 +0200

    Add implementation to error controller

--- beebiome-api/src/main/java/org/beebiome/dataportal/api/web/controller/BeeBiomeErrorController.java
+++ beebiome-api/src/main/java/org/beebiome/dataportal/api/web/controller/BeeBiomeErrorController.java
@@ -1,2 +1,47 @@
-package org.beebiome.dataportal.api.web.controller;public class BeeBiomeErrorController {
+package org.beebiome.dataportal.api.web.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BeeBiomeErrorController implements ErrorController {
+
+    private final static String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+    
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        HttpStatus status = getStatus(request);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", status == null? "No status": status.value() + ": " + status.getReasonPhrase());
+        body.put("exception", exception == null? "No exception": exception.getMessage());
+        return new ResponseEntity<>(body, status);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode != null) {
+            try {
+                return HttpStatus.valueOf(statusCode);
+            } catch (Exception ignored) {
+            }
+        }
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 }
