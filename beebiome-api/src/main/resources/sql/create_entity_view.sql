208e87d7787e2af87375750e3335bc89482c2053
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-11-10 12:10:37 +0100

    Fix SQL query creating entity view to retrieve sample without SRA experiment and/or without Nucleotide sequence

--- beebiome-api/src/main/resources/sql/create_entity_view.sql
+++ beebiome-api/src/main/resources/sql/create_entity_view.sql
@@ -21,13 +21,12 @@ SELECT s.biosampleAcc AS biosampleAcc,
 FROM sample s
          INNER JOIN projectToSample ps ON (ps.biosampleAcc = s.biosampleAcc)
          INNER JOIN project p ON (p.bioprojectAcc = ps.bioprojectAcc)
-         INNER JOIN biosamplePackage bp ON (bp.id = s.biosamplePackageId)
-         INNER JOIN sampleToExperiment se ON (se.biosampleAcc = s.biosampleAcc)
-         INNER JOIN experiment e ON (e.sraAcc = se.sraAcc)
          INNER JOIN species sp ON (sp.id = s.speciesId)
          INNER JOIN species h ON (h.id = s.hostSpeciesId)
-         INNER JOIN location l ON (l.id = s.locationId)
---          INNER JOIN sampleToNucleotide stn ON (stn.biosampleAcc = s.biosampleAcc)
+         LEFT JOIN sampleToExperiment se ON (se.biosampleAcc = s.biosampleAcc)
+         LEFT JOIN experiment e ON (e.sraAcc = se.sraAcc)
+         LEFT JOIN biosamplePackage bp ON (bp.id = s.biosamplePackageId)
+         LEFT JOIN location l ON (l.id = s.locationId)
 
 GROUP BY s.biosampleAcc, p.bioprojectAcc, bp.id, sp.id, h.id, l.name, s.collectionDate
 );
