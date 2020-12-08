b4b2c9bdade2b676f8257a990a1253fe881e25ea
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2020-12-08 11:12:18 +0100

    Fix HTML error (no <ul> in <p>)

--- beebiome-web/src/components/notfound.js
+++ beebiome-web/src/components/notfound.js
@@ -13,12 +13,11 @@ class Notfound extends React.Component {
             <div className={'row'}>
                 <div className={'col-sm-10 offset-sm-1'}>
                     <p>Sorry, but the page you were trying to view does not exist.</p>
-                    <p>It looks like this was the result of either:
-                        <ul>
-                            <li>a mistyped address</li>
-                            <li>an out-of-date link</li>
-                        </ul>
-                    </p>
+                    <p>It looks like this was the result of either:</p>
+                    <ul>
+                        <li>a mistyped address</li>
+                        <li>an out-of-date link</li>
+                    </ul>
                 </div>
             </div>
         </div>;
